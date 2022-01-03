928c14ae59e4aae680d38e30f936e14c327f724c
DevaZikrullah <zdeva804@gmail.com>
2022-01-03 22:17:16 +0700

    continue my project

--- src/assets/components/styled/Tabs.stylwd.js
+++ src/assets/components/styled/Tabs.stylwd.js
@@ -3,27 +3,36 @@ import styled from "styled-components";
 import Colors from "../../Colors";
 
 const TabsE1=styled.div`
+    width: 100%;
 `;
 
 const Title=styled.span`
+    padding:0.5rem ;
+    cursor: pointer;
+    position: relative;
+    ${p=>p.active?'':`color:${Colors.Gray};`}
 `;
 
 const Content=styled.div`
 `;
 
 const TitelsContainer=styled.span`
+    display: flex;
+    justify-content: space-around;
+    margin: 0 1.5rem;
+    padding: 0 1rem;
 `;
 
 export default function Tabs() {
     return (
         <TabsE1>
             <TitelsContainer>
-                <Title>Featured</Title>
+                <Title active>Featured</Title>
                 <Title>Collection</Title>
                 <Title>Arist</Title>
                 <Title>Tag</Title>
             </TitelsContainer>
-            <Content></Content>
+            <Content>Content</Content>
         </TabsE1>
     );
 }
